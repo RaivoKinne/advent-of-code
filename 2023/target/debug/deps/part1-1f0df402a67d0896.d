dd2566183e484d1ca7b74c42ceffa5ad41b8b72e
RaivoKinne <raivokinne236@gmail.com>
2023-12-01 21:43:23 +0200

    dezz balls

---
+++ 2023/target/debug/deps/part1-1f0df402a67d0896.d
@@ -0,0 +1,6 @@
+/home/nigga/Projects/rust/advent-of-code/2023/target/debug/deps/libpart1-1f0df402a67d0896.rmeta: day-01/src/bin/part1.rs day-01/src/bin/../../input1.txt
+
+/home/nigga/Projects/rust/advent-of-code/2023/target/debug/deps/part1-1f0df402a67d0896.d: day-01/src/bin/part1.rs day-01/src/bin/../../input1.txt
+
+day-01/src/bin/part1.rs:
+day-01/src/bin/../../input1.txt:
---
+++ 2023/target/debug/deps/part2-1d96a6a4e1ec6228.d
@@ -0,0 +1,6 @@
+/home/nigga/Projects/rust/advent-of-code/2023/target/debug/deps/libpart2-1d96a6a4e1ec6228.rmeta: day-01/src/bin/part2.rs day-01/src/bin/../../input2.txt
+
+/home/nigga/Projects/rust/advent-of-code/2023/target/debug/deps/part2-1d96a6a4e1ec6228.d: day-01/src/bin/part2.rs day-01/src/bin/../../input2.txt
+
+day-01/src/bin/part2.rs:
+day-01/src/bin/../../input2.txt:
